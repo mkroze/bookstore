7941dae2926d3bbe07c8d85632b4db906d3a7b1c
mkroze <mkblue1976@gmail.com>
2025-04-21 00:16:15 +0100

    adding http route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+console.log(request);
+return response.status(234).send("Welcome to the Mern Stack boy")
+})
+
 app.listen(PORT, () => {
   console.log(`app is listening to port : ${PORT}`);
 });
